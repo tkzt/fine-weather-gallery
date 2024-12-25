5d7cd6209468fdbcb1d313cd6441a55333261f92
Allen Tao <allen@tkzt.cn>
2024-12-25 23:53:15 +0800

    fix: fixes sharking problem during loading thumbnails

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -2,12 +2,12 @@
   <div class="
     cursor-pointer relative overflow-hidden rd-2 transition-170
     hover:scale-103
-  " ref="containerRef">
+  " ref="containerRef" :style="{ minHeight: cardSize[1] + 'px', minWidth: cardSize[0] + 'px' }">
     <Suspense>
       <template #default>
         <ImageAsync :src="imgSrc" class="
             w-100% block  dark:bg-[rgba(0,0,0,.2)] bg-[rgba(0,0,0,.1)]
-            backdrop-blur-4 saturate-120" :style="{ minHeight: minImageHeight + 'px' }" />
+            backdrop-blur-4 saturate-120" />
       </template>
       <template #fallback>
         <canvas class="w-100% rd-2 block" ref="skeletonRef" width="32" height="32"></canvas>
@@ -36,7 +36,9 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue';
+import {
+  computed, nextTick, onMounted, ref,
+} from 'vue';
 import { decode } from 'blurhash';
 import ImageAsync from './ImageAsync.vue';
 
@@ -56,25 +58,34 @@ const props = defineProps({
 });
 const skeletonRef = ref(null);
 const containerRef = ref();
+const cardSize = ref([0, 0]);
 
 const imgSrc = computed(
   () => `/thumbnail/${props.src}`,
 );
-const minImageHeight = computed(
-  () => Math.floor(
-    (containerRef.value?.offsetWidth || 0) * (props.blurHash.size[1] / props.blurHash.size[0]),
-  ),
-);
+// const minImageHeight = computed(
+//   () => Math.floor(
+//     (containerRef.value?.offsetWidth || 0) * (props.blurHash.size[1] / props.blurHash.size[0]),
+//   ),
+// );
 
 onMounted(() => {
   const originSize = props.blurHash.size;
-  skeletonRef.value.height = Math.floor((originSize[1] / originSize[0]) * 32);
-
+  if (originSize[0] >= originSize[1]) {
+    skeletonRef.value.height = Math.floor((originSize[1] / originSize[0]) * 32);
+  } else {
+    skeletonRef.value.width = Math.floor((originSize[0] / originSize[1]) * 32);
+  }
   const pixels = decode(props.blurHash.encoded, 32, 32);
   const ctx = skeletonRef.value.getContext('2d');
   const imageData = ctx.createImageData(32, 32);
   imageData.data.set(pixels);
   ctx.putImageData(imageData, 0, 0);
+
+  nextTick(() => {
+    const { width, height } = containerRef.value.getBoundingClientRect();
+    cardSize.value = [width, height];
+  });
 });
 </script>
 
